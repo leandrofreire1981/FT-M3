7abff598b3b7472f31761235d6cac04fe06ef60e
Leandro <freire.leandro@hotmail.com>
2022-09-08 18:35:38 -0300

    ejercicio 2

--- 04-AdvancePromises/homework/src/pledge.js
+++ 04-AdvancePromises/homework/src/pledge.js
@@ -5,6 +5,60 @@ Promises Workshop: construye la libreria de ES6 promises, pledge.js
 // // TU CÓDIGO AQUÍ:
 
 
+function $Promise(executor)  {
+    if(typeof executor !== 'function')
+        throw TypeError('executor must be a function')
+    this._state = 'pending'
+    this._handlerGroups = []
+   // this._callHandlers.bind(this)
+    executor(this._internalResolve.bind(this), this._internalReject.bind(this))
+}
+
+
+$Promise.prototype._internalResolve = function(data)  {
+  if(this._state==='pending') {
+    this._state = 'fulfilled'
+    this._value = data
+    this._callHandlers()
+  }
+}
+
+$Promise.prototype._internalReject = function(reson) {
+    if(this._state === 'pending') {
+        this._state='rejected'
+        this._value=reson
+        this._callHandlers()
+      }    
+    }
+
+$Promise.prototype.then = function(s1, e1) {
+  if(typeof s1 !== 'function' && typeof e1 !== 'function') {
+    s1 = e1 = false
+  }
+  this._handlerGroups.push({
+    successCb: s1,
+    errorCb: e1
+  })
+  
+  if(this._state!=='pending') this._callHandlers()
+}
+
+$Promise.prototype._callHandlers = function() {
+  while(this._handlerGroups.length) {
+    const hand = this._handlerGroups.shift()
+    if(this._state === 'fulfilled') {
+        if(hand.successCb) {
+          hand.successCb(this._value)
+        }
+    }
+    if(this._state === 'rejected'){
+        if(hand.errorCb){
+          hand.errorCb(this._value)
+        }
+    }
+  }
+}
+
 
 module.exports = $Promise;
 /*-------------------------------------------------------
