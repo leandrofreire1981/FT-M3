01d2224c8079882b37a7a45cc420ac6cbf07a832
Leandro <freire.leandro@hotmail.com>
2022-09-15 11:48:04 -0300

    homework

--- 07-AsyncAwait/Generators/homework/homework.js
+++ 07-AsyncAwait/Generators/homework/homework.js
@@ -1,6 +1,30 @@
-function fizzBuzzGenerator(max) {
+function* fizzBuzzGenerator(max) {
   // Tu código acá:
-
+  if (!max)
+    max='infinito'
+  console.log('arranca')
+  let count = 1
+  let divisor3
+  let divisor5
+  let mensaje
+  while(max==='infinito' || count <= max)
+  {
+    divisor3 = count % 3
+    divisor5 = count % 5
+    if(divisor3===0 && divisor5===0)
+      mensaje = 'Fizz Buzz'
+    if(divisor3===0 && divisor5!==0)
+      mensaje = 'Fizz'
+    if(divisor3!==0 && divisor5===0)
+      mensaje = 'Buzz'
+    if(divisor3!==0 && divisor5!==0)
+      mensaje = count
+    
+    yield mensaje
+    count++
+  } 
+  
+  
 }
 
 module.exports = fizzBuzzGenerator;
